64bc131af6f0a794984fa2fab961658539bb93ac
Rodrigo Nicolás Gómez <rodrigogmez14@gmail.com>
2025-09-27 00:18:46 -0300

    Correcciones archivo .env

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,20 @@ import Panel from './components/Panel/Panel.jsx'
 
 function App() {
 
+    console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+    console.log(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+    console.log(import.meta.env.VITE_FIREBASE_PROJECT_ID);
+    console.log(import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
+    console.log(import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
+    console.log(import.meta.env.VITE_FIREBASE_APP_ID);
+    console.log(import.meta.env.VITE_FIREBASE_MEASUREMENT_ID);
+    
+    
+    
+    
+    
+    
+
     return (
         <ThemeProvider>
             <CartProvider>
--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,22 @@ import {initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDG9StxycdiPqJ5kvsCL5qYvodEFHan-KU",
-    authDomain: "tp1-react-coderhouse.firebaseapp.com",
-    projectId: "tp1-react-coderhouse",
-    storageBucket: "tp1-react-coderhouse.firebasestorage.app",
-    messagingSenderId: "856672967354",
-    appId: "1:856672967354:web:b82ef593109db1fafd8083",
-    measurementId: "G-HLLBH4M7L2"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+console.log(firebaseConfig.apiKey);
+console.log(firebaseConfig.authDomain);
+console.log(firebaseConfig.projectId);
+console.log(firebaseConfig.storageBucket);
+console.log(firebaseConfig.messagingSenderId);
+console.log(firebaseConfig.appId);
+console.log(firebaseConfig.measurementId);
 
 const app = initializeApp(firebaseConfig);
 
